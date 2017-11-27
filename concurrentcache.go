019b1136928c5607c458ca3fd4639d964d735ab2
JC <jiachu_huang@163.com>
2017-11-27 10:00:06 +0800

    concurrent cache struct

--- concurrentcache.go
+++ concurrentcache.go
@@ -1,1 +1,20 @@
 package concurrentcache
+
+import (
+	"sync"
+	"time"
+)
+
+type ConcurrentCache struct {
+
+}
+
+type ConcurrentCacheSegment struct {
+	sync.RWMutex
+}
+
+type ConcurrentCacheNode struct {
+	v interface{}
+	lifeTime time.Duration
+	createTime time.Time
+}
--- queue.go
+++ queue.go
@@ -1,1 +1,13 @@
 package concurrentcache
+
+type Queue struct {
+	head *QNode
+	tail *QNode
+}
+
+type QNode struct {
+	prev *QNode
+	next *QNode
+	v interface{}
+}
+
