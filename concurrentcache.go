86953610266d96ab88b0b9d25f502cc001ee4a86
JC <jiachu_huang@163.com>
2017-11-28 14:20:11 +0800

    cache operator

--- concurrentcache.go
+++ concurrentcache.go
@@ -4,6 +4,7 @@ import (
 	"sync"
 	"time"
 	"errors"
+	"sync/atomic"
 )
 
 type ConcurrentCache struct {
@@ -13,8 +14,8 @@ type ConcurrentCache struct {
 
 type ConcurrentCacheSegment struct {
 	sync.RWMutex
-	lruQueue *Queue
-	data map[string]interface{}
+	data map[string]*ConcurrentCacheNode
+	lvPool map[string]*ConcurrentCacheNode
 	dCount uint32
 	dLen uint32
 	pool *sync.Pool
@@ -24,6 +25,7 @@ type ConcurrentCacheSegment struct {
 
 type ConcurrentCacheNode struct {
 	V interface{}
+	visit uint32
 	lifeExp time.Duration
 	createTime time.Time
 }
@@ -44,11 +46,221 @@ func NewConcurrentCache(sCount, dCount uint32) (*ConcurrentCache, error) {
 }
 
 func newConcurrentCacheSegment(dCount uint32) *ConcurrentCacheSegment {
-	lq := NewQueue()
 	pool := &sync.Pool{
 		New: func() interface{} {
-			return &ConcurrentCacheNode{lifeExp:0, createTime: time.Now()}
+			return &ConcurrentCacheNode{}
 		},
 	}
-	return &ConcurrentCacheSegment{lruQueue:lq, pool:pool, dCount:dCount, data: make(map[string]interface{}), dLen:0}
+	return &ConcurrentCacheSegment{lvPool:make(map[string]*ConcurrentCacheNode, 5), pool:pool, dCount:dCount, data: make(map[string]*ConcurrentCacheNode), dLen:0}
+}
+
+func (cc *ConcurrentCache) Set(key string, value interface{}, expire time.Duration) (bool, error) {
+	if key == "" || value == nil {
+		return false, errors.New("key or value can not be empty")
+	}
+	h := MurmurHash2(key)
+	h = h % cc.sCount
+	cs := cc.segment[h]
+	_, err := cs.set(key, value, expire, false)
+	if err != nil {
+		return false, err
+	}
+	return true, nil
+}
+
+func (cc *ConcurrentCache) Get(key string) (interface{}, error) {
+	if key == "" {
+		return false, errors.New("key can not be empty")
+	}
+	h := MurmurHash2(key)
+	h = h % cc.sCount
+	cs := cc.segment[h]
+	value, err := cs.get(key)
+	return value, err
+}
+
+func (cc *ConcurrentCache) Delete(key string) (bool, error) {
+	if key == "" {
+		return false, errors.New("key can not be empty")
+	}
+	h := MurmurHash2(key)
+	h = h % cc.sCount
+	cs := cc.segment[h]
+	_, err := cs.delete(key)
+	return true, err
+}
+
+func (cc *ConcurrentCache) Expire(key string, expire time.Duration) (bool, error) {
+	if key == "" {
+		return false, errors.New("key can not be empty")
+	}
+	h := MurmurHash2(key)
+	h = h % cc.sCount
+	cs := cc.segment[h]
+
+	cs.Lock()
+	defer cs.Unlock()
+
+	cn, exists := cs.data[key]
+	if !exists {
+		return true, nil
+	} else {
+		if cn.expire() || cn.createTime.Add(expire).Before(time.Now()) {
+			return true, nil
+		} else {
+			cn.lifeExp = expire
+			atomic.AddUint32(&cn.visit, 1)
+		}
+	}
+	return true, nil
+}
+
+func (cc *ConcurrentCache) Add(key string, value interface{}, expire time.Duration) (bool, error) {
+	if key == "" || value == nil {
+		return false, errors.New("key or value can not be empty")
+	}
+	h := MurmurHash2(key)
+	h = h % cc.sCount
+	cs := cc.segment[h]
+	ok, err := cs.set(key, value, expire, false)
+	if !ok && err == nil {
+		return false, nil
+	} else if err != nil {
+		return false, err
+	}
+	return true, nil
+}
+
+func (cs *ConcurrentCacheSegment) set(key string, value interface{}, expire time.Duration, nx bool) (bool, error) {
+	cs.Lock()
+	defer cs.Unlock()
+
+	var cn *ConcurrentCacheNode
+	cn, exists := cs.data[key]
+	if nx && exists && !cn.expire() {
+		return false, nil
+	}
+	if !exists {
+		if cs.dLen >= cs.dCount {
+			pk := cs.pick()
+			cn = cs.data[pk]
+		} else {
+			cn = cs.newConcurrentCacheNode()
+			cs.dLen++
+		}
+	}
+	cn.reset()
+	cn.V = value
+	cn.lifeExp = expire
+	return true, nil
+}
+
+func (cs *ConcurrentCacheSegment) pick() string {
+	again:
+	pl := len(cs.lvPool)
+	for k, v := range cs.data {
+		if pl >= 5 {
+			break
+		}
+		_, exists := cs.lvPool[k]
+		if !exists {
+			cs.lvPool[k] = v
+			pl++
+		}
+	}
+	var pk string
+	var pk_cn *ConcurrentCacheNode
+	for k, v := range cs.lvPool {
+		_, exists := cs.data[k]
+		if !exists {
+			delete(cs.lvPool, k)
+			continue
+		}
+		if pk_cn == nil {
+			if v.expire() {
+				delete(cs.lvPool, k)
+				return k
+			}
+			pk_cn = v
+			pk = k
+			continue
+		}
+		if v.expire() {
+			delete(cs.lvPool, k)
+			pk_cn = v
+			pk = k
+			return k
+		} else {
+			if v.visit < pk_cn.visit {
+				pk_cn = v
+				pk = k
+			}
+		}
+	}
+	if pk_cn == nil {
+		goto again
+	} else {
+		delete(cs.lvPool, pk)
+	}
+	return pk
+}
+
+func (cs *ConcurrentCacheSegment) get(key string) (interface{}, error) {
+	cs.RLock()
+	defer cs.RUnlock()
+
+	cn, exists := cs.data[key]
+	if !exists {
+		return nil, nil
+	}
+	if cn.expire() {
+		return nil, nil
+	} else {
+		atomic.AddUint32(&cn.visit, 1)
+		return cn.V, nil
+	}
+}
+
+func (cs *ConcurrentCacheSegment) delete(key string) (bool, error) {
+	cs.Lock()
+	defer cs.Unlock()
+
+	cn, exists := cs.data[key]
+	if !exists {
+		return true, nil
+	}
+	delete(cs.data, key)
+	if _, ok := cs.lvPool[key]; ok {
+		delete(cs.lvPool, key)
+	}
+	cs.dLen--
+	cs.pool.Put(cn)
+	return true, nil
+}
+
+func (cs *ConcurrentCacheSegment) newConcurrentCacheNode() *ConcurrentCacheNode {
+	cn := cs.pool.Get().(*ConcurrentCacheNode)
+	return cn
+}
+
+func (cs *ConcurrentCacheSegment) recycle(cn *ConcurrentCacheNode) {
+	if cn != nil {
+		cs.pool.Put(cn)
+	}
+}
+
+func (cn *ConcurrentCacheNode) reset() {
+	if cn != nil {
+		cn.V = nil
+		cn.createTime = time.Now()
+		cn.lifeExp = 0
+		cn.visit = 0
+	}
+}
+
+func (cn *ConcurrentCacheNode) expire() bool {
+	if cn.lifeExp == 0 {
+		return false
+	}
+	return cn.createTime.Add(cn.lifeExp).Before(time.Now())
 }
